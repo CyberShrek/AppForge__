f97a3607d27360df9faf836d478f7f6c9146d2fe
Александр Ильин <ilin.aleksandr@vniizht.ru>
2023-11-01 21:44:58 +0300

    Refactoring

--- src/main/resources/static/js/built/index.js
+++
@@ -1,2 +0,0 @@
-import{U as e}from"./UserCheck-19371e53.js";import"./wretch-40d1f204.js";import"./sweetAlert2-0948e87a.js";(new e).fetch().then((()=>{import("./IndexApplication-1778993a.js").then((e=>new e))}));
-//# sourceMappingURL=index.js.map
--- src/main/resources/static/js/built/userInfo-4f3aa991.js
+++
@@ -1,2 +0,0 @@
-import{U as e}from"./UserCheck-19371e53.js";let r={};async function t(){return r=await(new e).fetch()}export{t as r,r as u};
-//# sourceMappingURL=userInfo-4f3aa991.js.map
