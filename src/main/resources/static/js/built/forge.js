6d2f5ee1d2e34564f114b2bdee548f3357be398c
Александр Ильин <ilin.aleksandr@vniizht.ru>
2023-11-01 21:45:09 +0300

    Refactoring

---
+++ src/main/resources/static/js/built/forge.js
@@ -0,0 +1,2 @@
+import{r as e}from"./appInfo-8b4c1c18.js";import{r}from"./userInfo-4f3aa991.js";import"./UserCheck-19371e53.js";import"./wretch-40d1f204.js";import"./sweetAlert2-0948e87a.js";Promise.all([e(),r()]).then((()=>{import("./ForgedApplication-0eb96cf6.js").then((function(e){return e.b})).then((e=>{"complete"===document.readyState&&new e.ForgedApplication}))}));
+//# sourceMappingURL=forge.js.map
