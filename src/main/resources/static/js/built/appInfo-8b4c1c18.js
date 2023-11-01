f3ef6b1c7a31970bbe44628d8c65f5e1a3a4ce1a
Александр Ильин <ilin.aleksandr@vniizht.ru>
2023-11-01 21:42:48 +0300

    .gitignore is now working

---
+++ src/main/resources/static/js/built/appInfo-8b4c1c18.js
@@ -0,0 +1,2 @@
+import{J as e,v as s,n as r}from"./UserCheck-19371e53.js";class t extends e{constructor(){super(),this.path="/appforge/info",this.errorFooter="Не удалось загрузить информацию о приложении",this.headers={Code:s(r.code,"")}}}let a={};async function o(){return a=await(new t).fetch()}export{a,o as r};
+//# sourceMappingURL=appInfo-8b4c1c18.js.map
--- src/main/resources/static/js/built/index.js
+++ src/main/resources/static/js/built/index.js
@@ -1,2 +1,2 @@
-import{U as e}from"./UserCheck-dc22fa2d.js";import"./wretch-40d1f204.js";import"./modal-891a31fc.js";import"./sweetAlert2-0948e87a.js";(new e).fetch().then((()=>{import("./IndexApplication-f20f0e6b.js").then((e=>new e))}));
+import{U as e}from"./UserCheck-19371e53.js";import"./wretch-40d1f204.js";import"./sweetAlert2-0948e87a.js";(new e).fetch().then((()=>{import("./IndexApplication-1778993a.js").then((e=>new e))}));
 //# sourceMappingURL=index.js.map
